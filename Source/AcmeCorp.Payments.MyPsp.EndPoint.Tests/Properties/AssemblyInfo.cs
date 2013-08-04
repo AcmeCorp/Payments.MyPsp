cb221204b19d916a1a445e3173915c3653922176
CallumHibbert <callum.hibbert@gmail.com>
2013-08-04 22:42:38 +0100

    Solution skeleton.

---
+++ Source/AcmeCorp.Payments.MyPsp.EndPoint.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("AcmeCorp.Payments.MyPsp.EndPoint.Tests")]
+[assembly: AssemblyTitle("AcmeCorp.Payments.MyPsp.EndPoint.Tests")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("29d8f0a9-e69f-4118-9a12-bc3cc6bf487b")]
+[assembly: NeutralResourcesLanguage("en-GB")]
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")] // Assembly is delay signed.
+[assembly: SuppressMessage("Microsoft.Design", "CA1016:MarkAssembliesWithAssemblyVersion")] // Development version is "0.0.0.0"
+[assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")] // "AssemblyInformationalVersion" is a string
